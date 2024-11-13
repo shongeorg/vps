5e922d1d675ac78605a4c39a07c29a1910075511
shongeorg <144728538+shongeorg@users.noreply.github.com>
2024-11-13 17:20:14 +0200

    Create index.mjs

---
+++ index.mjs
@@ -0,0 +1,21 @@
+const { Telegraf } = require('telegraf');
+const ytdl = require('ytdl-core');
+
+const bot = new Telegraf('7062487698:AAE4B4ZuVDBMGC1WKzm-052hg2HqoZMePzc');
+
+bot.on('text', async (ctx) => {
+  const message = ctx.message.text;
+  if (ytdl.validateURL(message)) {
+    try {
+      const stream = ytdl(message, { filter: 'audioonly' });
+      ctx.replyWithAudio({ source: stream });
+    } catch (error) {
+      console.error(error);
+      ctx.reply('Сталася помилка при завантаженні аудіо.');
+    }
+  } else {
+    ctx.reply('Будь ласка, надішліть посилання на відео з YouTube.');
+  }
+});
+
+bot.launch();
